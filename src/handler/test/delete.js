54260e63032d5abe791ea838900647b2e65d358a
HuynhDung <huynhtandung97dev@gmail.com>
2023-05-03 11:30:49 +0700

    còn cái submission chưa fix xong

--- src/handler/test/delete.js
+++ src/handler/test/delete.js
@@ -3,30 +3,24 @@ const BadReqErr = require("../../error/bad-req");
 const Submission = require("../../model/submission");
 
 async function deleteTest(req, res, next) {
-  try {
-    // kiểm tra người tạo
-    const test = await Test.findById(req.params.id);
-    if (!test.createdBy.equals(req.user.id)) {
-      throw new BadReqErr(
-        "Bạn không phải người tạo bài thi"
-      );
+    try {
+        // kiểm tra người tạo
+        const test = await Test.findById(req.params.id);
+        if (!test.createdBy.equals(req.user.id)) {
+            throw new BadReqErr("Bạn không phải người tạo bài thi");
+        }
+
+        await Test.findByIdAndDelete(test._id);
+
+        for await (const s of test.submissions) {
+            await Submission.findByIdAndDelete(s);
+        }
+
+        res.status(204).json({});
+    } catch (err) {
+        console.log(err);
+        next(err);
     }
-
-    await Test.findByIdAndDelete(test._id);
-
-    for await (const s of test.submissions) {
-      await Submission.findByIdAndUpdate(s, {
-        $unset: {
-          test: 1,
-        },
-      });
-    }
-
-    res.status(204).json({});
-  } catch (err) {
-    console.log(err);
-    next(err);
-  }
 }
 
 module.exports = deleteTest;
--- src/handler/test/update.js
+++ src/handler/test/update.js
@@ -3,37 +3,55 @@ const Submission = require("../../model/submission");
 const Test = require("../../model/test");
 
 async function changeTest(req, res, next) {
-  try {
-    const test = await Test.findById(req.params.id);
-    if (!test) {
-      throw new BadReqErr("Không tồn tại bài thi");
+    try {
+        const test = await Test.findById(req.params.id);
+        if (!test) {
+            throw new BadReqErr("Không tồn tại bài thi");
+        }
+
+        await test.updateOne({
+            ...req.body,
+            members: req.body.memberIds,
+            class: req.body.classId,
+        });
+
+        //check member đã được thêm submission chưa, nếu chưa tiến hành add
+        for await (const m of test.members) {
+            const newSubmission = new Submission({
+                user: m,
+                test: test.id,
+                totalQuestion: req.body.questions.length || 0,
+            });
+            await newSubmission.save();
+
+            // Thêm bài nộp vào bài kiểm tra để tiện populate
+            await test.updateOne({
+                ...req.body,
+                members: req.body.memberIds,
+                class: req.body.classId,
+            });
+        }
+
+        await Submission.updateMany(
+            {
+                test: test._id,
+            },
+            {
+                $set: {
+                    totalQuestion: req.body.questions.length || 0,
+                },
+            }
+        );
+
+        const detail = await Test.findById(req.params.id);
+
+        res.json({
+            test: detail,
+        });
+    } catch (err) {
+        console.log(err);
+        next(err);
     }
-
-    await test.updateOne({
-      ...req.body,
-    });
-    await test.save();
-
-    await Submission.updateMany(
-      {
-        test: test._id,
-      },
-      {
-        $set: {
-          totalQuestion: req.body.questions.length || 0,
-        },
-      }
-    );
-
-    const detail = await Test.findById(req.params.id);
-
-    res.json({
-      test: detail,
-    });
-  } catch (err) {
-    console.log(err);
-    next(err);
-  }
 }
 
 module.exports = changeTest;
